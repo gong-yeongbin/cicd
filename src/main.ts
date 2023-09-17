d885bdaa8d4bfb1aa44335091ffbe5caaf2a4fee
yeongbin-gong <youngbin816@gmail.com>
2023-09-17 12:38:56 +0900

    feat(server-config): 환경변수설정추가
    nest 서버 환경 변수 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	const configService = app.get(ConfigService);
+	const port: number = configService.get('PORT');
+
+	await app.listen(port);
 }
+
 bootstrap();
