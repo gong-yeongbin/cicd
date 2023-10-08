26067bdacb55b4b47eadc5d2fe1dc496585c5db3
yeongbin-gong <youngbin816@gmail.com>
2023-10-08 14:52:39 +0900

    fix(package): 프로젝트 커밋 되돌리기
    에러로 인한 프로젝트 커밋 되돌리기

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-	constructor(private readonly configService: ConfigService) {}
+	constructor() {}
 	getHello(): string {
-		console.log(this.configService.get('postgre.username'));
-		console.log(this.configService.get('postgre.port'));
-		console.log(this.configService.get('postgre.password'));
-
 		return 'Hello World!';
 	}
 }
