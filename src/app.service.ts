30c7b040832068166e7896f3a2f80cde5edfe638
yeongbin-gong <youngbin816@gmail.com>
2023-10-18 12:06:35 +0900

    test(cd): nest app test
    nest app test

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,11 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { WINSTON_MODULE_NEST_PROVIDER, WinstonLogger } from 'nest-winston';
+import { Injectable } from '@nestjs/common';
+// import { WINSTON_MODULE_NEST_PROVIDER, WinstonLogger } from 'nest-winston';
 
 @Injectable()
 export class AppService {
-	constructor(
-		@Inject(WINSTON_MODULE_NEST_PROVIDER)
-		private readonly logger: WinstonLogger
-	) {}
+	constructor() {} // private readonly logger: WinstonLogger // @Inject(WINSTON_MODULE_NEST_PROVIDER)
 	getHello(): string {
-		this.logger.log(process.env.AWS_ACCESS_KEY);
+		// this.logger.log(process.env.AWS_ACCESS_KEY);
 		return 'Hello World!!!!!!';
 	}
 }
